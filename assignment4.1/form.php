37ce96743f5d9557a1d55550c1f15a40bb55ebb5
Erivat <emile.rivat@uvm.edu>
2014-05-08 22:51:26 -0400

    Update form.php

--- assignment4.1/form.php
+++ assignment4.1/form.php
@@ -29,6 +29,339 @@
      </ol>
 </nav>   
 
+
+<?php
+include ("top.php");
+
+$debug = false;
+
+if (isset($_GET["debug"])) {
+    $debug = true;
+}
+
+if ($debug)
+    print "<p>DEBUG MODE IS ON</p>";
+//default
+$fullName = "Sierra Thibodeau";
+$email = "sathibod@uvm.edu";
+$gender = "Female";
+$redSoxGame = "Yes";
+$redSoxPlayer = "Mike Napoli";
+$startingPitcher = true;
+$reliefPitcher = false;
+$closer = false;
+$firstBase = false;
+$secondBase = false;
+$thirdBaser = false;
+$shortStop = false;
+$catcher = false;
+$outfielder = false;
+$desginatedHitter = false;
+$basketball = true;
+$cricket = false;
+$football = true;
+$hockey = false;
+$rowing = false;
+$swimming = false;
+$futbol = false;
+$tennis = true;
+
+$yourURL = "https://www.uvm.edu/~sathibod/cs008/assignment3.1/form3.php";
+
+//errors
+$fullNameERROR = false;
+$emailERROR = false;
+$genderERROR = false;
+$redSoxGameERROR = false;
+$redSoxPlayerERROR = false;
+$startingPitcherERROR = true;
+$reliefPitcherERROR = false;
+$closerERROR = false;
+$firstBaseERROR = false;
+$secondBaseERROR = false;
+$thirdBaserERROR = false;
+$shortStopERROR = false;
+$catcherERROR = false;
+$outfielderERROR = false;
+$desginatedHitterERROR = false;
+$basketballERROR = false;
+$cricketERROR = false;
+$footballERROR = false;
+$hockeyERROR = false;
+$rowingERROR = false;
+$swimmingERROR = false;
+$futbolERROR = false;
+$tennisERROR = false;
+
+if (isset($_POST["btnSubmit"])) {
+
+//******************************************************************
+// is the refeering web page the one we want or is someone trying 
+// to hack in. this is not 100% reliable but ok for our purposes   */
+//
+    // Security check block one, no changes needed
+//check for errors
+    include ("validation_functions.php"); // you need to create this file (see link in lecture notes)
+    $errorMsg = array();
+
+    $dataRecord = array();
+//************************************************************
+// we need to make sure there is no malicious code so we do 
+// this for each element we pass in. Be sure your names match
+// your objects
+// 
+// Security check block two
+// 
+// What this does is take things like <script> and replace it
+// with &lt;script&gt; so that hackers cannot send malicous 
+// code to you.
+//   
+//  
+// */
+
+    $fullName = htmlentities($_POST["txtFullName"], ENT_QUOTES, "UTF-8");
+    $email = htmlentities($_POST["txtEmail"], ENT_QUOTES, "UTF-8");
+
+    if (isset($_POST["chkBasketball"])) {
+        $basketball = true;
+    } else {
+        $basketball = false;
+    }
+
+    if (isset($_POST["chkCricket"])) {
+        $cricket = true;
+    } else {
+        $cricket = false;
+    }
+    if (isset($_POST["chkFootball"])) {
+        $football = true;
+    } else {
+        $football = false;
+    }
+    if (isset($_POST["chkHockey"])) {
+        $hockey = true;
+    } else {
+        $hockey = false;
+    }
+    if (isset($_POST["chkRowing"])) {
+        $rowing = true;
+    } else {
+        $rowing = false;
+    }
+    if (isset($_POST["chkSwimming"])) {
+        $swimming = true;
+    } else {
+        $swimming = false;
+    }
+    if (isset($_POST["chkFutbol"])) {
+        $futbol = true;
+    } else {
+        $futbol = false;
+    }
+    if (isset($_POST["chkTennis"])) {
+        $tennis = true;
+    } else {
+        $tennis = false;
+    }
+    
+    if (isset($_POST["chkStartingPitcher"])) {
+        $startingPitcher = true;
+    } else {
+        $startingPitcher = false;
+    }
+
+    if (isset($_POST["chkReliefPitcher"])) {
+        $reliefPitcher = true;
+    } else {
+        $reliefPitcher = false;
+    }
+    if (isset($_POST["chkCloser"])) {
+        $closer = true;
+    } else {
+        $closer = false;
+    }
+
+    if (isset($_POST["chkFirstBase"])) {
+        $firstBase = true;
+    } else {
+        $firstBase = false;
+    }
+        if (isset($_POST["chkSecondBase"])) {
+        $secondBase = true;
+    } else {
+        $secondBase = false;
+    }
+
+    if (isset($_POST["chkThirdBase"])) {
+        $thirdBase = true;
+    } else {
+        $thirdBase = false;
+    }
+    if (isset($_POST["chkShortStop"])) {
+        $shortStop = true;
+    } else {
+        $shortStop = false;
+    }
+
+    if (isset($_POST["chkCatcher"])) {
+        $catcher = true;
+    } else {
+        $catcher = false;
+    }
+        if (isset($_POST["chkOutfield"])) {
+        $outfielder = true;
+    } else {
+        $outfielder = false;
+    }
+
+    if (isset($_POST["chkDesignatedHitter"])) {
+        $designatedHitter = true;
+    } else {
+        $designatedHitter = false;
+    }
+    $gender = htmlentities($_POST["radGender"], ENT_QUOTES, "UTF-8");
+
+    $year = htmlentities($_POST["Year"], ENT_QUOTES, "UTF-8");
+
+    //testing for correct format and adding to array
+    if ($fullName == "") {
+        $errorMsg[] = "Please enter your full name";
+        $firstNameERROR = true;
+    } elseif (!preg_match('/^[a-zA-Z\s]+$/', $fullName)) {
+        $errorMsg[] = "Your name appears to be incorrect.";
+        $firstNameERROR = true;
+    }
+    $dataRecord[] = $firstName;
+    
+    if ($email == "") {
+        $errorMsg[] = "Please enter your email address";
+        $emailERROR = true;
+    } elseif (!verifyEmail) {
+        $errorMsg[] = "Your email address appears to be incorrect.";
+        $emailERROR = true;
+    }
+    $dataRecord[] = $email;
+    
+    //add data to array
+    $dataRecord[] = $gender;
+    $dataRecord[] = $redSoxGamer;
+    $dataRecord[] = $redSoxPlayer;
+    $dataRecord[] = $startingPitcher;
+    $dataRecord[] = $reliefPitcher;
+    $dataRecord[] = $closer;
+    $dataRecord[] = $firstBase;
+    $dataRecord[] = $secondBase;
+    $dataRecord[] = $thirdBase;
+    $dataRecord[] = $shortStop;
+    $dataRecord[] = $catcher;
+    $dataRecord[] = $outfielder;
+    $dataRecord[] = $designatedHitter;
+    $dataRecord[] = $basketball;
+    $dataRecord[] = $cricket;
+    $dataRecord[] = $football;
+    $dataRecord[] = $hockey;
+    $dataRecord[] = $rowing;
+    $dataRecord[] = $swimming;
+    $dataRecord[] = $futbol;
+    $dataRecord[] = $tennis;
+    
+    
+
+    // our form data is valid at this point so we can process the data
+    if (!$errorMsg) {
+        if ($debug)
+            print "<p>Form is valid</p>";
+//########################################################
+        // Begin processing data
+        //########################################################
+        // Save Forms data to a csv file on the cloud
+        // NOTE: When you save the forms information to a file, the file 
+        // permissions can cause problems
+        //NOTE: my file is in a folder called data
+        // Step one in netbeans create new file, name it formData.csv
+        // Step two delete the contents of this csv file and save it
+        // Step three use fugu or winscp to set the permissions on this
+        //            file to 666 (rw-  for everyone)
+        // Now try your form and see if it saves.
+
+        $fileExt = ".csv";
+
+        $myFileName = "registration";
+
+        $filename = $myFileName . $fileExt;
+
+        if ($debug)
+            print "\n\n<p>filename is " . $filename;
+
+        // now we just open the file for append
+        $file = fopen($filename, 'a');
+
+        // write the forms informations
+        fputcsv($file, $dataRecord);
+
+        // close the file
+        fclose($file);
+
+        //####################################################################
+        $message = '<h2>Your information.</h2>';
+
+        foreach ($_POST as $key => $value) {
+
+            $message .= "<p>";
+
+            $camelCase = preg_split('/(?=[A-Z])/', substr($key, 3));
+
+            foreach ($camelCase as $one) {
+                $message .= $one . " ";
+            }
+            $message .= " = " . htmlentities($value, ENT_QUOTES, "UTF-8") . "</p>";
+        }
+        include_once('mailMessage.php');
+        $mailed = sendMail($email, $message);
+    } // ends if form was submitted. We will be adding more information ABOVE this
+}
+?>
+
+<article id="main">
+<?php
+//*****************************************************************************
+//
+//  In this block  display the information that was submitted and do not 
+//  display the form.
+//  
+//  NO CHANGES NEEDED
+//
+if (isset($_POST["btnSubmit"]) AND empty($errorMsg)) {  // closing of if marked with: end body submit
+    print "<h1>Your Request has ";
+
+    if (!$mailed) {
+        echo "not ";
+    }
+
+    echo "been processed</h1>";
+
+    print "<p>A copy of this message has ";
+    if (!$mailed) {
+        echo "not ";
+    }
+    print "been sent</p>";
+    print "<p>To: " . $email . "</p>";
+    print "<p>Mail Message:</p>";
+    echo $message;
+} else {
+    print '<div id="errors">';
+
+    if ($errorMsg) {
+        echo "<ol>\n";
+        foreach ($errorMsg as $err) {
+            echo "<li>" . $err . "</li>\n";
+        }
+        echo "</ol>\n";
+    }
+
+    print '</div>';
+    ?>
+
 <form action="/~erivat/cs008/finalProject/form1.php" method="post" id="frmRegister" enctype="multipart/form-data">
 
 <fieldset>               
